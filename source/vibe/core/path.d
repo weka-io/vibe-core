12daa5c5758b1de604411a8f7665d4248a3239a7
Sönke Ludwig <sludwig@rejectedsoftware.com>
2016-10-25 08:56:05 +0200

    Add Path concatenation operators.

--- source/vibe/core/path.d
+++ source/vibe/core/path.d
@@ -1,5 +1,7 @@
 module vibe.core.path;
 
+static import std.path;
+
 struct Path {
 	nothrow: @safe:
 	private string m_path;
@@ -12,6 +14,9 @@ struct Path {
 	string toString() const { return m_path; }
 
 	string toNativeString() const { return m_path; }
+
+	Path opBinary(string op : "~")(string subpath) { return this ~ Path(subpath); }
+	Path opBinary(string op : "~")(Path subpath) { return Path(std.path.buildPath(m_path, subpath.toString())); }
 }
 
 struct PathEntry {
