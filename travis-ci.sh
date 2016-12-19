003331cc90f875e02c75bf6b329c634ac86f5237
Sönke Ludwig <sludwig@rejectedsoftware.com>
2016-12-19 22:37:18 +0100

    Test with all Linux eventcore configurations.

--- travis-ci.sh
+++ travis-ci.sh
@@ -3,24 +3,25 @@
 set -e -x -o pipefail
 
 # test for successful release build
-dub build --combined -b release --compiler=$DC
+dub build -b release --compiler=$DC -c $CONFIG
 
 # test for successful 32-bit build
 if [ "$DC" == "dmd" ]; then
-	dub build --combined --arch=x86
+	dub build --arch=x86 -c $CONFIG
 fi
 
-dub test --combined --compiler=$DC
+dub test --compiler=$DC -c $CONFIG
 
 if [ ${BUILD_EXAMPLE=1} -eq 1 ]; then
     for ex in $(\ls -1 examples/); do
         echo "[INFO] Building example $ex"
+        # --override-config vibe-core/$CONFIG
         (cd examples/$ex && dub build --compiler=$DC && dub clean)
     done
 fi
 if [ ${RUN_TEST=1} -eq 1 ]; then
     for ex in `\ls -1 tests/*.d`; do
         echo "[INFO] Running test $ex"
-        dub --temp-build --compiler=$DC --single $ex
+        dub --temp-build --compiler=$DC --single $ex # --override-config vibe-core/$CONFIG
     done
 fi
