b6bedd63c024d0ff4ab1bec246a23c79fa2dff1c
Sönke Ludwig <sludwig@rejectedsoftware.com>
2020-03-18 15:32:52 +0100

    Update version to 1.9.0.

--- source/vibe/core/core.d
+++ source/vibe/core/core.d
@@ -1105,7 +1105,7 @@ void setTaskCreationCallback(TaskCreationCallback func)
 /**
 	A version string representing the current vibe.d core version
 */
-enum vibeVersionString = "1.8.1";
+enum vibeVersionString = "1.9.0";
 
 
 /**
