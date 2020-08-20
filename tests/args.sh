eef6be673be1000e27d8e9080660fa1844254463
Geod24 <pro.mathias.lang@gmail.com>
2020-08-21 00:41:23 +0900

    Workaround dub bug by using '--single'
    Recently, dub was upgraded from v1.18.0 to v1.22.0
    (the remote LATEST file was fixed), which caused half of the test-suite
    (all non select-based configs) to fail, because the 'args' test would
    somehow run the `0-tcp.d` file.
    This works around the problem by using `--single` explicitly.

--- tests/args.sh
+++ tests/args.sh
@@ -3,8 +3,8 @@
 set -e
 die() { echo "$@" 1>&2 ; exit 1; }
 
-( dub args.d | grep -q '^argtest=$' ) || die "Fail (no argument): '`dub args.d`'"
-( dub args.d --argtest=aoeu | grep -q '^argtest=aoeu$' ) || die "Fail (with argument): '`dub args.d --argtest=aoeu`'"
-( ( ! dub args.d --inexisting 2>&1 ) | grep -qF 'Unrecognized command line option' ) || die "Fail (unknown argument): '`dub args.d --inexisting 2>&1`'"
+( dub --single args.d | grep -q '^argtest=$' ) || die "Fail (no argument): '`dub --single args.d`'"
+( dub --single args.d -- --argtest=aoeu | grep -q '^argtest=aoeu$' ) || die "Fail (with argument): '`dub --single args.d -- --argtest=aoeu`'"
+( ( ! dub --single args.d -- --inexisting 2>&1 ) | grep -qF 'Unrecognized command line option' ) || die "Fail (unknown argument): '`dub --single args.d -- --inexisting 2>&1`'"
 
 echo 'OK'
